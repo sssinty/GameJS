9556580042d674650fb7610fbff85996877dfc5f
KirillLaptop <sinty.kpov@gmail.com>
2023-08-20 20:07:41 +0300

    solution modules homeworck

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+console.log('app worked!');
+import game, {readGameSaving, loadGame, saveGame} from "./game";
+
+
+const game = new Game();
+game.start();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            // {
+            //     test: /\.js$/,
+            //     loader: 'raw-loader'
+            // },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ]
+}
