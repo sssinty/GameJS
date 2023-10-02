ba5479e150972f47ccba8e73b7931036cf0d390f
KirillLaptop <sinty.kpov@gmail.com>
2023-10-02 10:51:31 +0300

    add new folders and configs

---
+++ src/__test__/Character.test.js
@@ -0,0 +1,13 @@
+import Daemon from '../js/Class/Daemon';
+
+test('checking the operation of the parent class and methods', () => {
+  const newHero = new Daemon('name');
+  expect(newHero).toEqual(newHero);
+  newHero.levelUp();
+  newHero.damage(10);
+  expect(() => new Daemon('name', 'Masfafer')).toThrow('Такого типа не существует!');
+  expect(() => new Daemon('Aksmfxvoeasfmrascxzd')).toThrow('Имя должно содержать от 2 до 10 символов!');
+  newHero.damage(3000);
+  expect(() => { newHero.levelUp(); }).toThrow('Нельзя повысить уровень умершего!');
+  expect(() => { newHero.damage(300); }).toThrow('Персонаж мертв!');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
-const path = require('path');
+// const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            // {
-            //     test: /\.js$/,
-            //     loader: 'raw-loader'
-            // },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin()
-    ]
-}
+  mode: 'development',
+  module: {
+    rules: [
+      // {
+      //     test: /\.js$/,
+      //     loader: 'raw-loader'
+      // },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
